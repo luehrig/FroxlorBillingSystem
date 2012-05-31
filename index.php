7964cb11bdc465af79b3584ad5a72dd290cdf678
Max Luehrig <max.luehrig@bymaxe.de>
2012-06-01 00:35:57 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<p>This page uses frames. The current browser you are using does not support frames.</p>
+<?php
+	echo 'Hello World';
+?>
+</body>
+</html>
