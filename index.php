736345eaa75805296053ac11924aa76150d97942
Jana Keim <Jana_Keim@web.de>
2012-06-01 16:38:27 +0200

    includes, bild, css

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<?php 
+// 	Header
+	include("View/header_view.php");
+	
+// 	Menu
+	include("View/menu_view.php");
+	
+// 	Content
+	include("View/home_view.php");
+	
+// 	Footer
+	include("View/footer_view.php");
+?>
 
-<link rel="stylesheet" href="css/style.css" type="text/css">
-
-<script language="javascript" src="js/jquery-1.7.2.min.js"></script>
-
-</head>
-<body>
-<div class="header">Kopf</div>
 <div class="content_container">huhu
 	<div class="content"></div>
 	<div class="advertisment"></div>
 </div>
 
 
-<?php
-	echo 'Hello World blaaaaaaaaaaaaaaa';
-?>
+
 </body>
 </html>
