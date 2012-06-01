d0cf360ecd8ca6dcebbf57fe08763dcfb2e38aa3
Jana Keim <Jana_Keim@web.de>
2012-06-01 13:43:04 +0200

    schnickschnack

--- index.php
+++ index.php
@@ -4,15 +4,21 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
 
-<link rel="stylesheet" href="/css/style.css" type="text/css">
+<link rel="stylesheet" href="css/style.css" type="text/css">
 
-<script language="javascript" src="/js/jquery-1.7.2.min.js"></script>
+<script language="javascript" src="js/jquery-1.7.2.min.js"></script>
 
 </head>
 <body>
-<p>This page uses frames. The current browser you are using does not support frames.</p>
+<div class="header">Kopf</div>
+<div class="content_container">huhu
+	<div class="content"></div>
+	<div class="advertisment"></div>
+</div>
+
+
 <?php
-	echo 'Hello World';
+	echo 'Hello World blaaaaaaaaaaaaaaa';
 ?>
 </body>
 </html>
