0032685d78b0afcde6aeda2dccca936682bcb5e3
Max Luehrig <max.luehrig@bymaxe.de>
2012-06-13 23:04:23 +0200

    seo friendly urls for core content pages in frontend

--- menu.php
+++ menu.php
@@ -1,9 +1,9 @@
 <div class='mainmenu'>
 	<ul>
-	   <li class='active '><a href='index.php?content=home'><span><?php echo VIEW_MENU_HOME; ?></span></a></li>
-	   <li><a href='index.php?content=products'><span><?php echo VIEW_MENU_PRODUCTS; ?></span></a></li>
-	   <li><a href="index.php?content=help" class="lightbox"><span><?php echo VIEW_MENU_HELP; ?></span></a></li>
-	   <li><a href='index.php?content=imprint'><span><?php echo VIEW_MENU_IMPRINT; ?></span></a></li>
+	   <li class='active '><a href='page-home.html'><span><?php echo VIEW_MENU_HOME; ?></span></a></li>
+	   <li><a href='page-products.html'><span><?php echo VIEW_MENU_PRODUCTS; ?></span></a></li>
+	   <li><a href="page-help.html" class="lightbox"><span><?php echo VIEW_MENU_HELP; ?></span></a></li>
+	   <li><a href='page-imprint.html'><span><?php echo VIEW_MENU_IMPRINT; ?></span></a></li>
 	   
 	   <li id="mainmenuelement"><a href="customercenter" id="start_customercenter"><span><?php echo VIEW_MENU_CUSTOMERCENTER?></span></a></li>
 	   
