080c63630ae54f2d9ae3c657ee346a60ff113c54
Max Luehrig <max.luehrig@bymaxe.de>
2012-06-15 12:00:00 +0200

    google ajax crawling support added in mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -1,3 +1,20 @@
 <div class="content_container">
 
+<?php 
+/*
+ * this coding is neccessary to support google ajax crawling
+ * 
+ * receive special masked url and print content as static page
+ */
+if(isset($_GET['_escaped_fragment_'])) {
+	
+	$action = 'show_'. substr($_GET['_escaped_fragment_'], 5);
+	
+	$language_id = language::ISOTointernal($_GET['lang']);
+	
+	include_once 'logic/process_content_handling.php';
+}
+
+?>
+
 </div>
