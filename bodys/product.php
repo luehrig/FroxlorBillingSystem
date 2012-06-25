f1bca55d564c2c8c4b32d8daee349bfa6d252cda
unknown <Erol@.(none)>
2012-06-25 20:26:06 +0200

    dynamische produkterzeugung nach anzahl der produkte

--- bodys/product.php
+++ bodys/product.php
@@ -9,104 +9,73 @@ require_once PATH_CLASSES .'cl_product_attribute.php';
 $content = new content(1);
 
 
-//TODO: hier kommt ein Fehler! Fatal error: Class'produkt' not found
-// // new product($product_id, $language_id)
-// $product = new product(1, 1);
+// new product($product_id, $language_id)
+$product = new product(1, 1);
 
-// $product_title = $product->getTitle();
-// $contract_periode = $product->getContractPeriode();
-// $description = $product->getDescription();
-// $quantity = $product->getQuantity();
-// $price = $product->getPrice();
+$product_title = $product->getTitle();
+$contract_periode = $product->getContractPeriode();
+$description = $product->getDescription();
+$quantity = $product->getQuantity();
+$price = $product->getPrice();
 
-// // array($ttribute_description => $value)
-// $attribute_value_map = array();
+// array($ttribute_description => $value)
+$attribute_value_map = array();
 
-// // return = array(atribute_id => Value);  getAttributesByProductId($product_id)
-// $product_info_map = productInfo::getAttributesByProductId(1);
+// return = array(atribute_id => Value);  getAttributesByProductId($product_id)
+$product_info_map = productInfo::getAttributesByProductId(1);
 
-// // return = array(atribute_id => descriptione);  getAllExistingAttrByLang($language_id)
-// $existing_attributes = productAttribute::getAllExistingAttrByLang(1);
+// return = array(atribute_id => descriptione);  getAllExistingAttrByLang($language_id)
+$existing_attributes = productAttribute::getAllExistingAttrByLang(1);
 
-// foreach($product_info_map as $atribute_id => $Value){
-// 	$attribute_value_map[$existing_attributes[$atribute_id]] = $Value;
-// }
+foreach($product_info_map as $atribute_id => $Value){
+	$attribute_value_map[$existing_attributes[$atribute_id]] = $Value;
+}
+
+$numberOfProducts = 7;
+$index = 1;
 
 
+
+
+echo '
+	<h1><?php echo VIEW_MENU_PRODUCTS; ?></h1>
+	<div class="boxwrapper">
+';
+
+
+while ($index <= $numberOfProducts) {
+	
+echo'
+	<div class="whitebox box_3inRow">
+		<img ID="littlelogo" src="images/logos/logo.png">
+		<fieldset>
+			<legend>
+				'. $product_title.'
+			</legend>
+			<p>'.$description.'</p>
+		</fieldset>
+		<button class="buttonlayout_buy" rel="'.$index.'">'.BUTTON_ADD_TO_CART.'</button>
+		<div id="book'.$index.'" class="slidebox">
+			<fieldset>
+				<legend>
+					Details
+				</legend>
+				
+				<ul>
+  					<li>Vertragslaufzeit'.$contract_periode.'</li>
+  					<li>Space '.$quantity.'</li>
+  					<li>Preis '.$price.'</li>
+				</ul>
+			</fieldset>
+		</div>
+<!-- TODO: rel tag has to content the product id! -->			
+		<button class="buttonlayout_more" rel="'.$index.'">'.BUTTON_MORE.'</button>
+	</div>
+
+';
+	$index++;
+}
+echo'</div>'
 
 ?>
-		
-
-		
-<h1><?php echo VIEW_MENU_PRODUCTS; ?></h1>
-<div class="boxwrapper">
-
-	<!-- Product No.1 -->
-	<div class="whitebox box_3inRow">
-		<img ID="littlelogo" src="images/logos/logo.png">
-		<fieldset>
-			<legend>
-				Produktname
-			</legend>
-			<p><?php echo $content->getText()?></p>
-		</fieldset>
-		<button class="buttonlayout_buy" rel="1"><?php echo BUTTON_ADD_TO_CART; ?></button>
-		<div id="book1" class="slidebox">
-			<fieldset>
-				<legend>
-					Details
-				</legend>
-				<?php echo $content->getText()?>
-			</fieldset>
-		</div>
-<!-- TODO: rel tag has to content the product id! -->			
-		<button class="buttonlayout_more" rel="1"><?php echo BUTTON_MORE; ?></button>
-	</div>
-				
-				
-	<!-- Product No.2 -->
-	<div class="whitebox box_3inRow">
-		<img ID="littlelogo" src="images/logos/logo.png">
-		<fieldset>
-			<legend>
-				Produktname
-			</legend>
-			<p><?php echo $content->getText()?></p>
-		</fieldset>
-		<button class="buttonlayout_buy" rel="2"><?php echo BUTTON_ADD_TO_CART; ?></button>
-		<div id="book2" class="slidebox">
-			<fieldset>
-				<legend>
-					Details
-				</legend>
-				<?php echo $content->getText()?>
-			</fieldset>
-		</div>
-<!-- TODO: rel tag has to content the product id! -->			
-		<button class="buttonlayout_more" rel="2"><?php echo BUTTON_MORE; ?></button>
-	</div>
-				
-	<!-- Product No.3 -->
-	<div class="whitebox box_3inRow">
-		<img ID="littlelogo" src="images/logos/logo.png">
-		<fieldset>
-			<legend>
-				Produktname
-			</legend>
-			<p><?php echo $content->getText()?></p>
-		</fieldset>
-		<button class="buttonlayout_buy" rel="3"><?php echo BUTTON_ADD_TO_CART; ?></button>
-		<div id="book3" class="slidebox">
-			<fieldset>
-				<legend>
-					Details
-				</legend>
-				<?php echo $content->getText()?>
-			</fieldset>
-		</div>
-<!-- TODO: rel tag has to content the product id! -->			
-		<button class="buttonlayout_more" rel="3"><?php echo BUTTON_MORE; ?></button>
-	</div>
-				
-</div>
 
