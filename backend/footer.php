34c3dd533de96c398ee27627ad19390d21dc1614
unknown <Erol@.(none)>
2012-06-29 20:27:09 +0200

    little whitelogo fix

--- backend/footer.php
+++ backend/footer.php
@@ -1,8 +1,8 @@
 <div class="footer">
 	
-	<div class="footer_copyright">
+	
 		&copy; by Max L&uumlhrig, Jana Keim, Josh Frey, Erol G&uumll. All rights reserved.
-	</div>
+	
 	<img ID="froxcloud_white" src="../images/logos/froxcloud_white.png">
 	
 </div>
