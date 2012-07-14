6beac2014c01fba44064ecc427645cf55e169693
erolguel <guel.erol@gmail.com>
2012-07-14 15:43:03 +0200

    new mobile view

--- mobile_menu.php
+++ mobile_menu.php
@@ -27,19 +27,19 @@ $site_language = language::getBrowserLanguage();
 include_once 'includes/languages/'. strtoupper($site_language) .'.inc.php';
 
 $cart = new shoppingcart(session_id());
-
 ?>
+
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
 <html>
 <head>
-
 <meta name="viewport" content="minimum-scale=1.0, width=device-width, maximum-scale=0.6667, user-scalable=no">
 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Froxcloud</title>
 
 <!-- stylesheets for lightbox, printer and desktop screen -->
-<link rel="stylesheet" href="css/style.css" media="screen and (min-device-width: 600px)" type="text/css" />
+<link rel="stylesheet" href="css/handheld.css" media="screen and (min-device-width: 600px)" type="text/css" />
 <link rel="stylesheet" href="css/print.css" type="text/css" media="print">
 <link rel="stylesheet" href="css/colorbox.css" type="text/css">
 
@@ -60,44 +60,43 @@ $cart = new shoppingcart(session_id());
 <script language="javascript" src="js/customercenter.js"></script>
 
 <link rel="SHORTCUT ICON" href="images/logos/favicon.ico" type="image/x-icon">
-
 </head>
+
 <body>
-<div class="header">
-	<img ID="logo" src="images/fcloud.png">
-	<div class="hide_for_mobile">
+<div class="mainmenu">
+	<ul>
 		
-		<div class="mobilemenu">
-	<a href="mobile_menu.php?lang=<?php echo $site_language; ?>"><span><?php echo "MENU"; ?></span></a>
-		</div>
+	   <li><a href="index.php?lang=<?php echo $site_language; ?>" id="home" class="nav mm_active"><span><?php echo VIEW_MENU_HOME; ?></span></a></li>
+	   <li><a href="index.php?lang=<?php echo $site_language; ?>" id="products" class="nav"><span><?php echo VIEW_MENU_PRODUCTS; ?></span></a></li>
+	   
+	   <!-- menu link for contact form if javascript is active (form will be opened in a popup) -->
+	   <div id="contact_form_js"></div>
+		<script type="text/javascript">
+			document.getElementById("contact_form_js").innerHTML='<li><a href="content_js.html?lang=<?php echo $site_language; ?>" class="lightbox"><span><?php echo VIEW_MENU_CONTACT; ?></span></a></li>';
+		</script>
 		
-	</div>
-	<div class="header_right">
-
-				<a href="shoppingcart.html?lang=<?php echo $site_language; ?>" id="shoppingcart" class="nav"><?php echo VIEW_MENU_SHOPPING_CART; ?> (<span id="current_cart_quantity"><?php echo $cart->getNumberOfProducts(); ?></span>)</a>
-	
-		<br>
-		
-		<div ID="customer_header">
- 		<div ID="customer_header_ajax"></div> <!-- Container for customer head when page is reloaded -->
-		<?php 
-			if(customer::isLoggedIn(session_id())) {
-				$customer = new customer($_SESSION['customer_id']);
-				$data = $customer->getData();
-				
-				echo '<div class="welcome_text">'. MSG_CUSTOMER_WELCOME .', '. $data['first_name'] .' '. $data['last_name'] .'!</div>';
-				echo '<a href="#" id="logout"><img src="images/logout.png" id="logout" title="'. BUTTON_LOGOUT_CUSTOMER .'"></a>';
-			}	
-		?>
-		
-		</div>
-		
-		<div class="language_switch">
-			<button class="language_button active" ID="german"> deutsch
-			</button>
-			<button class="language_button"  ID="english"> englisch
-			</button>
-		</div>
-
-	</div>
+		<!-- menu link for contact form if javascript is not active -->
+	   <li><noscript><a href="index.php?lang=<?php echo $site_language; ?>" id="contact" class="nav"><span><?php echo VIEW_MENU_CONTACT; ?></span></a></noscript></li>
+	   
+	   <li><a href="index.php?lang=<?php echo $site_language; ?>" id="help" class="nav"><span><?php echo VIEW_MENU_HELP; ?></span></a></li>
+	   <li><a href="index.php?lang=<?php echo $site_language; ?>" id="imprint" class="nav"><span><?php echo VIEW_MENU_IMPRINT; ?></span></a></li>
+	   
+	   
+	   <?php 
+	    /*
+	     * decide if customer is logged in or not
+	     * if customer is logged in add "nav" class to customer center link
+	     */
+	   	if (customer::isLoggedIn(session_id()) == true ) {
+	   		echo '<li id="mainmenuelement"><a href="#!page=customercenter&lang='. $site_language .'" id="customercenter" class="nav"><span>'. VIEW_MENU_CUSTOMERCENTER .'</span></a></li>';
+	   	}
+	   	else {
+	   		echo '<li id="mainmenuelement"><a href="#!page=customercenter&lang='. $site_language .'" id="customercenter"><span>'. VIEW_MENU_CUSTOMERCENTER .'</span></a></li>';
+	   	}
+	   ?>
+	   
+	</ul>
 </div>
+
+</body>
+</html>
