90d996a8d2bf28482ba620c9a518d932da881793
Max <max.luehrig@bymaxe.de>
2012-07-15 19:53:53 +0200

    enhancements in product overview

--- bodys/product.php
+++ bodys/product.php
@@ -3,6 +3,7 @@ if(!isset($_GET['page'])) {
 	include_once '../configuration.inc.php';
 }
 
+require_once PATH_CLASSES .'cl_customizing.php';
 require_once PATH_CLASSES .'cl_language.php';
 require_once PATH_CLASSES .'cl_product.php';
 require_once PATH_CLASSES .'cl_product_info.php';
@@ -11,6 +12,8 @@ require_once PATH_CLASSES .'cl_product_attribute.php';
 $content = new content(1);
 
 $language_id = language::ISOTointernal(language::getBrowserLanguage());
+
+$customizing = new customizing($language_id);
 
 $numberOfProducts = 7;
 $index = 0;
@@ -31,7 +34,7 @@ foreach ($active_product_array as $product_id => $product_data){
 	// return = array(atribute_id => Value); 
 	$product_info_map = productInfo::getAttributesByProductId($product_id);
 	
-	// return = array(atribute_id => descriptione);  
+	// return = array(atribute_id => description);  
 	$existing_attributes = productAttribute::getAllExistingAttrByLang($language_id);
 	
 	foreach($product_info_map as $attribute_id => $value){
@@ -62,12 +65,17 @@ foreach ($active_product_array as $product_id => $product_data){
 				</legend>
 	
 				<ul>
-	<li>'.PRODUCT_CONTRACT_PERIODE.' '.$product_data['contract_periode'].'</li>
-	<li>'.PRODUCT_QUANTITY.' '.$product_data['quantity'].'</li>
-	<li>'.PRODUCT_PRICE.' '.$product_data['price'].'</li>';
+	<li>'.PRODUCT_CONTRACT_PERIODE.': '.$product_data['contract_periode'].' '. LABEL_PRODUCT_CONTRACT_PEROIDE_UNIT .'</li>
+	<li>'.PRODUCT_PRICE.': '.$product_data['price'].' '. LABEL_PRODUCT_PRICE .'</li>';
 	
-	foreach($attribute_value_map as $attr_name => $value){
-		$return_string = $return_string. '<li>'. $attr_name .' '. $value .'</li>';
+	foreach($attribute_value_map as $attr_name => $value) {
+		if(array_search($attr_name, $existing_attributes) == $customizing->getCustomizingValue('sys_product_attribute_discspace') ) {
+			$return_string = $return_string. '<li>'. $attr_name .': '. $value . LABEL_PRODUCT_DISCSPACE_UNIT .'</li>';
+		}
+		else {
+			$return_string = $return_string. '<li>'. $attr_name .': '. $value .'</li>';
+		}
+		
 	}
 	$value_array = array($product_id, PRODUCT_DETAILS_LESS, PRODUCT_DETAILS_MORE);
 	$trimmed_values = implode(',', $value_array);
